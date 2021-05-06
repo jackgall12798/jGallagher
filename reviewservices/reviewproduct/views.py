5651fc6d8934125b4bbd326b354944632c5fb017
student <minigallagher.jg@gmail.com>
2021-05-06 13:34:50 +0100

    Reviews added to product page as defined in model through a loop

--- reviewservices/reviewproduct/views.py
+++ reviewservices/reviewproduct/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
+from .models import Review
 def home(request) :
      return render(request, 'reviewproduct/home.html', {'title':'Home Page '})
 def about(request) :
     return render(request, 'reviewproduct/about.html', {'title':'About Us'})
 def contact(request) :
     return render(request, 'reviewproduct/contact.html', {'title':'Contact Us'})
-def product(request) :
-    return render(request, 'reviewproduct/product.html', {'title':'Product Page'})
+#def product(request) :
+ #   return render(request, 'reviewproduct/product.html', {'title':'Product Page'})
 
-#def product (request) :
-#    product_info= {
-#       :  Product.objects.all()
-#    }
-# Create your views here.
+def product(request) :
+       review_info= {
+       'reviews': Review.objects.all()
+       }
+       return render(request, 'reviewproduct/product.html', review_info)
+
+
